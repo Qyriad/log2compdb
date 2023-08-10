5a122865be63468768cabf56cd5dc17f5141207c
Qyriad <qyriad@qyriad.me>
2023-08-10 15:08:33 -0600

    testing: make bare `pytest` work, making ./test.py unneeded

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import os.path
-import sys
-
-import pytest
-
-if __name__ == "__main__":
-    sys.path.insert(0, os.path.dirname(__file__))
-    pytest.main(["tests", *sys.argv])
