6532ef32b9e794ae653d99465c459e41abab6619
Qyriad <qyriad@qyriad.me>
2023-08-31 14:21:37 -0600

    greatly improve flake.nix

--- flake.nix
+++ flake.nix
@@ -14,21 +14,57 @@
         format = "pyproject";
         src = ./.;
 
-        buildInputs = with pkgs.python3Packages; [
+        meta = {
+          description = "Generate compile_commands.json from a build log with compiler invocations";
+          homepage = "https://github.com/Qyriad/log2compdb";
+          license = pkgs.lib.licenses.mit;
+
+          # In theory, this works anywhere Python does.
+          platforms = pkgs.python3.meta.platforms;
+        };
+
+        nativeBuildInputs = with pkgs.python3Packages; [
           setuptools
           wheel
         ];
+
+        nativeCheckInputs = with pkgs.python3Packages; [
+          pytest
+        ];
+
+        checkPhase = "pytest";
       };
+
+      devShellPkgs = with pkgs.python3Packages; [
+        build
+        twine
+      ];
 
     in {
 
-      devShells.default = pkgs.mkShell {
-        packages = [
-          log2compdb
-        ];
-      };
       packages.default = log2compdb;
-      applications.default = log2compdb;
+
+      devShells.default = pkgs.mkShell {
+        packages = devShellPkgs;
+        inputsFrom = [ log2compdb ];
+      };
+
+      devShells.user = pkgs.mkShell {
+
+        meta = {
+          description = "Like devShells.default, but will exec $USERSHELL.";
+        };
+
+        packages = devShellPkgs;
+        inputsFrom = [ log2compdb ];
+
+        shellHook = ''
+          [[ -z "$USERSHELL" ]] && \
+            echo 'Set $USERSHELL to use `nix shell`/`nix develop` with your preferred shell' ; \
+            exit 1
+          exec $USERSHELL
+        '';
+      };
     }
   );
 }
