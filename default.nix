5ec52bc049822f19e0c8cc759d4afb41bc64d088
Qyriad <qyriad@qyriad.me>
2025-09-20 17:56:22 +0200

    packaging: expose extra python versions

--- default.nix
+++ default.nix
@@ -1,3 +1,25 @@
 {
   pkgs ? import <nixpkgs> { },
-}: pkgs.python3Packages.callPackage ./package.nix { }
+  python3Packages ? pkgs.python3Packages,
+  qpkgs ? let
+    src = fetchTarball "https://github.com/Qyriad/nur-packages/archive/main.tar.gz";
+  in import src { inherit pkgs; },
+}: let
+  inherit (qpkgs) lib;
+
+  log2compdb = python3Packages.callPackage ./package.nix { };
+
+  overrideCall = scope: scope.callPackage log2compdb.override { };
+  # nb: the parentheses here are unnecessary but it looks fucking weird.
+  notDisabled = p: !(p.meta.disabled or false);
+
+  byPythonVersion = lib.pipe qpkgs.pythonScopes [
+    (lib.mapAttrs (lib.const overrideCall))
+    (lib.filterAttrs (lib.const notDisabled))
+  ];
+
+in log2compdb.overrideAttrs (prev: {
+  passthru = prev.passthru or { } // {
+    inherit byPythonVersion;
+  };
+})
--- flake.nix
+++ flake.nix
@@ -1,18 +1,34 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      flake = false;
+    };
     flake-utils.url = "github:numtide/flake-utils";
+    qyriad-nur = {
+      url = "github:Qyriad/nur-packages";
+      flake = false;
+    };
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    qyriad-nur,
   }: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs { inherit system; };
-    log2compdb = import ./default.nix { inherit pkgs; };
+    qpkgs = import qyriad-nur { inherit pkgs; };
+    inherit (qpkgs) lib;
+
+    log2compdb = import ./default.nix { inherit pkgs qpkgs; };
+
+    extraVersions = lib.mapAttrs' (pyName: value: {
+      name = "${pyName}-cappy";
+      inherit value;
+    }) log2compdb.byPythonVersion;
 
   in {
-    packages = {
+    packages = extraVersions // {
       default = log2compdb;
       inherit log2compdb;
     };
